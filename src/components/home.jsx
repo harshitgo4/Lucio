21f416aeaae6a4c769c5e41e1eca662fbbe8129d
harshit Goyal <harshitgo0409@gmail.com>
2023-08-12 18:56:01 +0530

    responsive

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -12,12 +12,12 @@ const Home = () => {
   return (
     <>
     
-      <Box>
+      <Box maxH={['60vh','100vh']}>
         <Mycarousel />
       </Box>
-    <Container maxWidth={'container.xl' } minHeight={'100vh'} p='16' marginTop={['-600','-100']}>
+    <Container maxWidth={'container.xl' } minHeight={'100vh'} p='16' marginTop={['00','100']}>
     <Heading textTransform={'uppercase'} py='2' w={'fit-content'} borderBottom={'2px solid'} margin={'auto'}>Services</Heading>
-    <Stack h={'full'} direction={['column']} p={'4'} alignItems={'center'} justifyContent={'space-evenly'}>
+    <Stack h={'full'} direction={['column']} p={'4'} alignItems={'center'} justifyContent={'space-evenly'} zIndex={'overlay'}>
         <Image src={img4} size h={'400'}/>
         <Text lineHeight={'160%'} letterSpacing={'widest'} py={['4','16']} >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla vitae voluptatibus laboriosam quidem excepturi ea similique id ipsa quasi quo. Iure provident beatae, aperiam, ut quidem explicabo et quasi ex officiis ipsa, aliquam saepe Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati tenetur nisi eligendi voluptas quidem sint, quam suscipit deserunt earum officiis, dolores fuga maxime temporibus provident. Fugit sequi in tempore. Quae, nobis necessitatibus.! 
@@ -30,28 +30,30 @@ const Home = () => {
 
 const Mycarousel = () => {
   return (
-    <Carousel autoPlay infiniteLoop interval={3000} showStatus={false} showArrows={false} showIndicators ={false} >
 
-        <Box w={['full']} h={'auto'}>
-          <Image src={img1}  alt="Image 1" />
-          <Heading  className={'hober'}  bg={'whiteAlpha.500'} color={'blackAlpha.600'}>
-           Computer Tutorials
-          </Heading>
-        </Box>
-   
-        <Box w={'full'} h={'100vh'}>
-          <Image src={img2} alt="Image 2" />
-          <Heading className={'hober'}bg={'blackAlpha.600'} >
-          Elecrical Tutorials
-          </Heading>
-        </Box>
-        <Box w={'full'} h={'100vh'}>
-          <Image src={img3} alt="Image 3" />
-          <Heading className={'hober'} bg={'blackAlpha.600'}>
-           Civil Tutorials
-          </Heading>
-        </Box>
-    </Carousel>
+        <Carousel   autoplay  infiniteLoop interval={3000} showStatus={false} showArrows={false} showIndicators ={true} swipeScrollTolerance={false} preventMovementUntilSwipeScrollTolerance="false" >
+
+<Box w={['full']} h={['50vh','100vh']}>
+  <Image src={img1}  alt="Image 1" />
+  <Heading  className={'hober'}  bg={'whiteAlpha.500'} color={'blackAlpha.600'}>
+   Computer Tutorials
+  </Heading>
+</Box>
+
+<Box w={'full'} h={['50vh','100vh']}>
+  <Image src={img2} alt="Image 2" />
+  <Heading className={'hober'}bg={'blackAlpha.600'} >
+  Elecrical Tutorials
+  </Heading>
+</Box>
+<Box w={'full'} h={['50vh','100vh']}>
+  <Image src={img3} alt="Image 3" />
+  <Heading className={'hober'} bg={'blackAlpha.600'}>
+   Civil Tutorials
+  </Heading>
+</Box>
+</Carousel>
+  
   );
 };
 
